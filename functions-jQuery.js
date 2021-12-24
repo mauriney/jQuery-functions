d810f69b2a5c56f219c59fc8f9e315130d7bf35d
Mauriney <maurineyy@gmail.com>
2021-12-24 10:43:44 -0300

    addClass(), removeClass(), toggleClass()

---
+++ functions-jQuery.js
@@ -0,0 +1,20 @@
+   //addClass(), removeClass(), toggleClass()
+
+   // Adicionar classe:
+    $("#adicionar_classe").click(function(){
+        $("#paragrafo-classes").addClass("styling");
+    });
+
+    // Remover classe:
+    $("#remover_classe").click(function(){
+        $("#paragrafo-classes").removeClass("styling");
+    });
+
+    // Alternar classe:
+    $("#alternar_classe").click(function(){
+        $("#paragrafo-classes").toggleClass("styling");
+    });
+
+
+
+    
