cc3d0c93bd148e5511a8ec017d5398d4e982d958
Mauriney <maurineyy@gmail.com>
2022-06-09 13:30:24 -0300

    updtate mask cpf

--- functions-jQuery.js
+++ functions-jQuery.js
@@ -1,4 +1,20 @@
-   //addClass(), removeClass(), toggleClass()
+//Function para criar uma máscara no input que impede de inserir outros caracteres que não seja numeros e mantém o formato de 11 digitos (xxx.xxx.xxx-xx). Precisa tratar os pontos caso não utilize os no banco de dados.
+function mascaraCPF(i){
+    let v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+}
+
+
+
+//addClass(), removeClass(), toggleClass()
 
    // Adicionar classe:
     $("#adicionar_classe").click(function(){
